6afcf8cda98b9f353373efbca22839da118a2be5
taofiq_subair <taofiq.subair@interswitchng.com>
2025-01-03 00:28:10 +0100

    added file input script

--- tag.py
+++ tag.py
@@ -8,12 +8,25 @@ view_arn = 'arn:aws:resource-explorer-2:us-east-1:339712936044:view/all-resource
 ec2_filter = 'service:ec2'
 s3_filter = 'service:s3'
 region_filter = 'region:us-east-1'
+file_path = 'tags.txt'
 
 # Get AWS Account ID
 def get_account_id():
   sts_client = boto3.client('sts')
   identity = sts_client.get_caller_identity()
   return identity['Account']
+
+def read_tag_file(path):
+  tags = {}
+  with open(path, "r") as file:
+    for line in file:
+      line = line.strip()
+      if line and '=' in line:
+        key, value = line.split("=",1)
+        tags[key.strip()] = value.strip()
+  return tags
+
+print(read_tag_file(file_path))
 
 # Display timestamp
 def log_message():
@@ -24,7 +37,7 @@ def get_all_res_arns(view):
   client = boto3.client('resource-explorer-2')
   arn_list = []
   next_token = None
-  filters = {'FilterString': region_filter+' '+ec2_filter}
+  filters = {'FilterString': region_filter+' '+s3_filter}
   # filters = None
 
   try:
@@ -53,8 +66,8 @@ def tag_resources():
   tagging_client = boto3.client('resourcegroupstaggingapi', region_name=region) if region else boto3.client("resourcegroupstaggingapi")
   all_arns = list(dict.fromkeys(get_all_res_arns(view_arn)))
 
-  if len(all_arns) == 0:
-    print("No resources found!")
+  if len(all_arns) == 0 :
+    print("No resources found to tag!")
     return
   else:
     for arn in all_arns:
@@ -62,15 +75,40 @@ def tag_resources():
         response = tagging_client.tag_resources(
           ResourceARNList=[arn],
           Tags={
-            key:value,
+            key:value
               }
           )
         print(f"{log_message()} -- Successfully tagged: {arn}")
       except Exception as e:
         print(f"{log_message()} -- Failed to tag {arn}. Error: {e}")
 
+def tag_resources_from_file():
+  tags = read_tag_file(file_path) if file_path else None
+  tagging_client = boto3.client('resourcegroupstaggingapi', region_name=region) if region else boto3.client("resourcegroupstaggingapi")
+  all_arns = list(dict.fromkeys(get_all_res_arns(view_arn)))
+
+  if len(all_arns) == 0 :
+    print("No resources found to tag!")
+    return
+  else:
+    for arn in all_arns:
+      if tags:
+        try:
+          response = tagging_client.tag_resources(
+            ResourceARNList=[arn],
+            Tags=tags
+            )
+          print(f"{log_message()} -- Successfully tagged: {arn}")
+        except Exception as e:
+          print(f"{log_message()} -- Failed to tag {arn}. Error: {e}")
+      else:
+        print("No tags in file!")
+        return
+
 def main(): 
-  tag_resources()
+  # tag_resources()
+  tag_resources_from_file()
 
 if __name__ == "__main__":
   main()
+  # pass
